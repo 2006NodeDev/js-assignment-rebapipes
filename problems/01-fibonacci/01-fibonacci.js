cf3d50910147cfa86dcfa56548a608cd441f1a7a
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-17 14:18:40 -0500

    Update 01-fibonacci.js
    fixed fib challenge

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,21 +1,19 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
-    let fib = (n) => {
-        if (n === 1) {
-            return 0
-        }
-        if (n === 2) {
-            return 1
-        }
-    
-        return fib (n - 1) + fib (n - 2)
-    }
+function fib(n, memo) {
+    memo = memo || {};
+
+    if (memo[n]) return memo[n];
+    if (n <= 1) return 1;
+
+    return memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
 }
 
 console.log(fib(1));
 console.log(fib(2));
 console.log(fib(3));
 console.log(fib(4));
-console.log(fib(10))
+console.log(fib(10));
+console.log(fib(100));
+console.log(fib(1000))
