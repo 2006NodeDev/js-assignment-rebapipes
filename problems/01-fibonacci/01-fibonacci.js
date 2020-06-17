0468c699aca8011e50b9d5bb574f3f6c81eb0cd6
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-17 14:10:29 -0500

    Update 01-fibonacci.js
    fix fib 1

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,20 +2,15 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-    let memo = { //memo method
-        1:0,
-        2:1,
-    }
-
     let fib = (n) => {
+        if (n === 1) {
+            return 0
+        }
+        if (n === 2) {
+            return 1
+        }
     
-        if(memo[n] || memo[n] === 0){
-            return memo[n]
-        }
-        else{
-            memo[n] = fib(n - 1) + fib(n - 2)
-            return memo[n]
-        }
+        return fib (n - 1) + fib (n - 2)
     }
 }
 
@@ -23,6 +18,4 @@ console.log(fib(1));
 console.log(fib(2));
 console.log(fib(3));
 console.log(fib(4));
-console.log(fib(10));
-console.log(fib(100));
-console.log(fib(1000))
+console.log(fib(10))
