d3d8743dd69973934ebce71d5437576efdbe6917
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-18 12:46:27 -0500

    Update 03-reverse-string.js
    began reverse-string.js

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,8 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    const someStr = ['one', 'two', 'three'];
+    return someStr;
 }
+
+reverseStr(someStr);
