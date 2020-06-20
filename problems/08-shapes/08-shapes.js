5e3bfacbbbf1cb5dc74b020beb8e102351d241d0
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-20 15:42:39 -0500

    need to fix shapes.js

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,53 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
+  switch (shape) {
+    case "Square":
+      let line = ""
+      for (let i = 1; i <= height; ++i) {
+          line += character
+      }
+      for (let i = 1; i <= height; ++i) {
+        console.log(line)
+      }
+      break;
+      
+    case "Triangle":
+      let line = ""
+      for (let i = 1; i <= height; ++i) {
+        line += character
+        console.log(line)
+      }
+      break;
   
-}
+    case "Diamond":
+      x = (height - 1) / 2
+      for (let i = 0; i <= x; i++) {
+        printDiamond(2 - i, 1 + 2 * i, character)
+      }
+      for (let i = 1; i < 3; i++) {
+        printDiamond(i, -2 * i + 5, character)
+      }
+      break;
+  
+    default:
+      console.log("Invalid shape")
+      break;
+    }
+  }
+
+  function printDiamond(shape, height, character){
+    let line = ""
+    for (let i = 0; i < shape; i++) {
+      line += ""
+    }
+    for (let i = 0; i < height; i++) {
+      line += character
+    }
+    for (let i = 0; i < character; i++) {
+      line += ""
+    }
+    console.log(line)
+  }
+
+printShape("Diamond", 5, "*");
