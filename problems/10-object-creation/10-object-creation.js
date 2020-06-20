5e346d3863194c6ebf3ccd9706e36ba945e7453d
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-20 16:15:15 -0500

    finished 10-object-creation.js

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,24 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+// Object Literal
+let dog = {name: "Indie", age: 12};
+console.log(dog);
+
+// Constructor
+let frog = Object();
+frog.name = "Hemingway"
+frog.age = 2
+console.log(frog);
+
+// ES6 Classes
+class Dog {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+let dog2 = new Dog ('Gabby', 10)
+console.log(dog2);
