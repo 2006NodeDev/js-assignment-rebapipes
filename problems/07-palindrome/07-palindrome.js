84e9d445945a81d9e2734fb624fae032e9119fd2
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-20 13:14:56 -0500

    finished palindrome js

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,19 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    let myStr = true;
+        for (let i = 0; i <= someStr.length - 1; i++) {
+            if (someStr[i] !== someStr[someStr.length - i - 1]) {
+                myStr = false
+            }
+        } if (myStr == false) {
+            return false
+        } else {
+            return true
+        } 
 }
+
+console.log(isPalindrome('ma\'am'));
+console.log(isPalindrome('not a Palindrome'));
+console.log(isPalindrome('bob'));
+console.log(isPalindrome('test for Palindrome'));
