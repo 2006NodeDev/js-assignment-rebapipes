36d100543a258a64181f29e07347289e295b085e
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-20 12:32:44 -0500

    finished even-number js

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,13 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    if((someNum & 1) == 0) {
+        return true
+    }
+    return false;
 }
+
+console.log(isEven(4));
+console.log(isEven(-25));
+console.log(isEven(0));
+console.log(isEven(333));
