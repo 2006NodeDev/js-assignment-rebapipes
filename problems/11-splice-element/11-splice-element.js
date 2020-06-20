1b0437ce18db41c817aac33fdad270ed0e89a66b
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-20 15:51:05 -0500

    finished 11-splice-element.js

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,8 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+  return someArr.splice(0, index)
 }
+
+let exampleArr = ['dog', 4, 'cat', 5, 'fish', 6, 'rabbit', 7]
+console.log(spliceElement(exampleArr, 8));
