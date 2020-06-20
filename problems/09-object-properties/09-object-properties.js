ae7ba6fa08b9fc92000da9d9cffc014a993aa4f0
rebapipes <51538500+rebapipes@users.noreply.github.com>
2020-06-20 15:42:18 -0500

    finished 09-object-properties.js

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,13 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    for(dog in someObj){
+        let name = someObj.name
+        let age = someObj.age
+        let breed = someObj.breed
+    return console.log(`${name} is a ${age} year old ${breed}.`);
+    }
 }
+
+let dog = {name: 'Indie', breed: 'corgi', age: 12,};
+(objectProperties(dog));
